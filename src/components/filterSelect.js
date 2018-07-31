d9b21055294f18aee14a53110fdf00b318e3874d
zhenglong <399615007@qq.com>
2018-07-31 17:58:39 +0800

    修改

--- src/components/filterSelect.js
+++ src/components/filterSelect.js
@@ -4,10 +4,11 @@ import '../assets/style/common.less'
 import '../assets/style/mixin.less'
 import '../assets/style/filterStyle.less'
 var classNames = require('classnames');
+
 class FilterSelect extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
+        this.classState = {
             nolimit:true,
             select:{
                 code:'',
@@ -15,29 +16,27 @@ class FilterSelect extends React.Component {
             }
         };
     }
-    isactive(){
 
-    }
+
     btnFilterType(code,name){
         let obj={
             code:code,
             name:name
         }
-        this.props.changeOption(obj)
-        console.log(code)
+        code==''?this.classState.nolimit=true:this.classState.nolimit=false;
+        this.classState.select.code=code;
+        this.props.changeOption(obj);
+
     }
+
     render() {
-        let spanState=classNames({
-            'filter-tag':true,
-            'active':true
-        })
-        const list=this.props.filterOption.map((item,index)=> {return <li key={item.code+''+index}><span  className="filter-tag" onClick={()=>{
+        const list=this.props.filterOption.map((item,index)=> {return <li key={item.code+''+index}><span  className={['filter-tag',item.code===this.classState.select.code && 'active'].join(' ')} onClick={()=>{
             this.btnFilterType(item.code,item.name)
         }}>{item.name}</span></li>})
         return (
             <div className='filter-item'>
                 <ul>
-                    <li><span  className={spanState} onClick={()=>this.btnFilterType('','不限')}>不限</span></li>
+                    <li><span  className={['filter-tag',this.classState.nolimit && 'active'].join(' ')} onClick={()=>this.btnFilterType('','不限')}>不限</span></li>
                     {list}
                 </ul>
             </div>
--- src/page/about.js
+++ src/page/about.js
@@ -32,6 +32,7 @@ class about extends Component{
         const filterOption1=[{  value:0, name:'全部' },{ value:1, name:'有效'},{ value:2, name:'无效' }]
         const filterOption2=[{ value:1, name:'春'},{ value:2, name:'秋' }]
         const filterOption3=[{ code:'primary_school', name:'小学'},{ code:'middle_school', name:'初中' },{ code:'high_school', name:'高中' }]
+        const filterOption4=[{ code:'primary_school1', name:'小学'},{ code:'middle_school1', name:'初中' },{ code:'high_school1', name:'高中' }]
 
 
         return(
@@ -43,6 +44,7 @@ class about extends Component{
                 <FilterBtn filterOption={filterOption1} changeOption={(state,option)=>this.changeOption(state,'Option1')}></FilterBtn>
                 <FilterBtn filterOption={filterOption2} changeOption={(state,option)=>this.changeOption(state,'Option2')}></FilterBtn>
                 <FilterSelect filterOption={filterOption3} changeOption={(state,option)=>this.changeOption(state,'Option3')}></FilterSelect>
+                <FilterSelect filterOption={filterOption4} changeOption={(state,option)=>this.changeOption(state,'Option3')}></FilterSelect>
 
             </div>
         )
